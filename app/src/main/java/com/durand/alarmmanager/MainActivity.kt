631fe13d110f57c32e4937d11280474cd6b89bbc
JosueDurand96 <josuedurandf@gmail.com>
2022-08-19 03:04:58 -0500

    setAlarm

--- app/src/main/java/com/durand/alarmmanager/MainActivity.kt
+++ app/src/main/java/com/durand/alarmmanager/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.durand.alarmmanager
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.getSystemService
 import com.durand.alarmmanager.databinding.ActivityMainBinding
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
@@ -12,9 +17,11 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var picker: MaterialTimePicker
     private lateinit var calendar: Calendar
+    private lateinit var alarManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -37,7 +44,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setAlarm() {
-
+        alarManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReceiver::class.java)
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent,0)
+        alarManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,pendingIntent
+        )
+        Toast.makeText(this,"Alarm set success", Toast.LENGTH_SHORT).show()
     }
 
     private fun showTimePicker() {
